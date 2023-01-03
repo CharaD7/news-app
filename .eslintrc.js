b585123611f17c1a561457f88b4ea2742fefc9c0
CharaD7 <jijakahn6@gmail.com>
2023-01-03 12:47:00 +0000

    Added more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,15 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'next',
+    'plugin:import/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended'
   ],
+  globals: {
+    React: 'readonly',
+  },
   overrides: [
   ],
   parser: '@typescript-eslint/parser',
@@ -16,10 +22,28 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
+    '@next/next',
     'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
+    'simple-import-sort'
   ],
   rules: {
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        },
+      }
+    ],
     indent: [
       'error',
       2
@@ -28,13 +52,22 @@ module.exports = {
       'error',
       'unix'
     ],
+    'max-lines': ['error', 80],
+    '@typescript-eslint/no-use-before-define': 'warn',
     quotes: [
       'error',
       'single'
     ],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
     semi: [
       'error',
       'always'
     ]
-  }
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
 };
